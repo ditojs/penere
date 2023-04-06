71184e7a54beb0d6ac32191d441f5cdf25e0e40a
Jürg Lehni <juerg@scratchdisk.com>
2023-04-06 12:46:04 +0200

    MOD: member chain

---
+++ tests/format/js/ mods/member-chain/member-chain.js
@@ -0,0 +1,27 @@
+object1.test().bla().bla().bla()
+
+object1
+  .test().bla().bla().bla()
+
+object1.test().bla().bla().bla().veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongFunctionName()
+
+file.stream
+  .off('data', onData)
+  .off('end', onEnd)
+  .pipe(stream)
+
+expect(format(integer, { locale: 'de-DE', date: true, time: true }))
+  .toBe('2. Januar 1970 um 11:17:36')
+
+function test() {
+  expect(
+    deindent`
+      some
+        indented
+          text`
+  ).toBe(
+`some
+  indented
+    text`
+  )
+}
