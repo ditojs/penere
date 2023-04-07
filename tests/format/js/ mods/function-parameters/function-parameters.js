03aa458919e80ed5ca430ce46724f87c5e377129
Jürg Lehni <juerg@scratchdisk.com>
2023-04-07 19:40:25 +0200

    MOD: function parameters

---
+++ tests/format/js/ mods/function-parameters/function-parameters.js
@@ -0,0 +1,121 @@
+
+async function request(api, { bla }, {
+  url,
+  method = 'get',
+  params = null,
+  query = params || null,
+  headers = null,
+  data = null
+}) {
+  //
+}
+
+
+async function request(api, {
+  bla //
+}, {
+  url,
+  method = 'get',
+  params = null,
+  query = params || null,
+  headers = null,
+  data = null
+}) {
+  //
+}
+
+async function request(api, {
+  url, //
+  method,
+  params,
+  query,
+  headers,
+  data
+}) {
+  //
+}
+
+request(api, {
+  url,
+  method: 'get',
+  params: null,
+  query: params
+})
+
+request(api, { bla }, {
+  url,
+  method: 'get',
+  params: null,
+  query: params
+})
+
+const relation = new RelationController(
+  this, object, relationInstance, relationDefinition
+)
+
+async function mapConcurrently1(input, callback,
+  { concurrency = 0 }
+) {
+}
+
+async function mapConcurrently2(input, callback,
+  { concurrency = 0 } = {}
+) {
+}
+
+async function mapConcurrently3(
+  input,
+  callback,
+  { concurrency = 0 }
+) {
+}
+
+async function mapConcurrently4(
+  input,
+  callback,
+  { concurrency = 0 } = {}
+) {
+}
+
+const mapConcurrently5 = (input, callback,
+  { concurrency = 0 }
+) => {
+}
+
+const mapConcurrently6 = (input, callback,
+  { concurrency = 0 } = {}
+) => {
+}
+
+const mapConcurrently7 = (
+  input,
+  callback,
+  { concurrency = 0 }
+) => {
+}
+
+const mapConcurrently8 = (
+  input,
+  callback,
+  { concurrency = 0 } = {}
+) => {
+}
+
+function test(foo, bar) {
+}
+
+function test(
+foo, bar
+) {
+}
+
+function test(
+foo,
+bar
+) {
+}
+
+function test(
+foo
+) {
+}
