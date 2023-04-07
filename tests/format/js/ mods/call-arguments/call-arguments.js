28ad1faae28430fee946648fef5e78c4fc2a680f
Jürg Lehni <juerg@scratchdisk.com>
2023-04-07 19:43:51 +0200

    MOD: call arguments

---
+++ tests/format/js/ mods/call-arguments/call-arguments.js
@@ -0,0 +1,31 @@
+describe.each(['foobar.com', 'www.foobar.com',])(
+  'isUrl(%o)',
+  str => {
+    it('returns true', () => {
+      expect(isUrl(str)).toBe(true)
+    })
+  }
+)
+
+const value = format(
+  isDate(value) || value == null ? value : new Date(value),
+  { locale, date, time }
+)
+
+test(foo(), bar())
+
+test(foo(),
+bar())
+
+test(
+foo(),
+bar())
+
+test(
+foo(),
+bar()
+)
+
+test(
+foo()
+)
