2eb338d03f5396f6e1e344468d06398b28b1b8f1
Jürg Lehni <juerg@scratchdisk.com>
2023-11-20 23:35:50 +0100

    MOD: ternary

---
+++ tests/format/js/ mods/ternary/ternary.js
@@ -0,0 +1,21 @@
+const value1 = one ? true : two ? three : four
+
+const value2 = oneVeryLongName ? true : twoVeryLongName ? threeVeryLongName : fourVeryLongName
+
+const value3 = one
+  ? true
+  : two
+  ? three
+  : four
+
+const value4 = one
+  ? true
+  : two
+    ? three
+    : four
+
+const value5 = one
+  ? true
+  : two ?? null
+    ? three
+    : four
